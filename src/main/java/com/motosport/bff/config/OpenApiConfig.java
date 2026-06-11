d55a755affdcfb02d87dc7f2bc7f5587f96894ee
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-06-10 21:42:33 -0400

    agregue config y controller al bff

---
+++ src/main/java/com/motosport/bff/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.motosport.bff.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI motosportOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("MotoSport BFF API")
+                        .description("Backend For Frontend para MotoSport")
+                        .version("v1")
+                        .contact(new Contact()
+                                .name("MotoSport Team")
+                                .email("dev@motosport.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")));
+    }
+}
